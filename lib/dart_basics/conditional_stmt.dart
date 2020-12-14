057332e36c6b101826cc667a2f5f5f0ed96845a1
Grishma Shrestha <grishmashrestha@gmail.com>
2020-12-14 17:56:32 +0545

    Learn lists

---
+++ lib/dart_basics/conditional_stmt.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num1 = 2;
+  int num2 = 3;
+  if (num1 == num2) {
+    print("true");
+  } else if (num1 > num2) {
+    print("greater");
+  } else if (num1 < num2) {
+    print("smaller");
+  }
+}
---
+++ lib/dart_basics/lists.dart
@@ -0,0 +1,7 @@
+void main() {
+  const ages = [10, 23, 22];
+  List myList = List();
+  myList = [11, 21, 31];
+  myList.addAll(ages);
+  print(myList);
+}
